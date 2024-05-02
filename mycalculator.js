7683385b28ff2e02237d37e517af6d817bec8dc0
Anson Reyes <ansonreyes12345@gmail.com>
2024-05-02 01:12:49 -0400

    Final build

--- mycalculator.js
+++ mycalculator.js
@@ -1,3 +1,4 @@
+//object literal that holds the functions for the equations
 const calculator = 
 {
     add : function(a,b)
@@ -36,37 +37,127 @@ const calculator =
 };
 
 const buttons = document.querySelectorAll("button");
+let input; // Used for user input that isn't the operators
+let operation; // For the operators
+let first; // For the first group numbers in an equation
+let second; // For the second group numbers in an equation
+let isFirstRemoved = false; // For testing whether or not text should be removed
 
+//For loop that creates event listeners for all of the buttons
 for(let i=0; i < buttons.length; i++)
 {
     buttons[i].addEventListener("click",()=>
     {
         let text = document.querySelector("#input");
 
-        if(buttons[i].innerText == '=')
-        {
-            let breakpoint = /[+\-*/]/;
-            let textinput = text.value.toString();
-            let operation = textinput.split(breakpoint);
-            let sign;
-            for(let j=0; j < textinput.length; j++)
-            {
-                if(textinput[j] == '+' || textinput[j] == '-' || textinput[j] == '*' || textinput[j] == '/')
-                {
-                    sign = textinput[j];
-                }
-            }
 
-            text.value = calculator.operate(operation[0],sign,operation[1]);
+        if(buttons[i].innerText == '=' && typeof first !== 'undefined' && text.value.toString() !== '')
+        {
+            second = parseFloat(text.value);
+            if(operation == '/' && second == 0)
+            {
+                text.value = "You cannot divide by zero! Press clr now to enter new values."
+                first = undefined;
+                second = undefined;
+                operation = '';
+            }
+            else
+            {
+            text.value = calculator.operate(first,operation,second);
+            operation = '';
+            }
+        }
+        else if(buttons[i].innerText == '+' && text.value.toString() !== '')
+        {
+            
+            if(operation === '' || typeof(operation) == 'undefined')
+            {
+                first = parseFloat(text.value);
+                isFirstRemoved = false;
+                operation = '+';
+            }
+            else
+            {
+                second = parseFloat(text.value);
+                text.value = calculator.operate(first,operation,second);
+                operation = '';
+            }
+        }
+        else if(buttons[i].innerText == '-' && text.value.toString() !== '')
+        {
+            if(operation == '' || typeof(operation) == 'undefined')
+            {
+                first = parseFloat(text.value);
+                isFirstRemoved = false;
+                operation = '-';
+            }
+            else
+            {
+                second = parseFloat(text.value);
+                text.value = calculator.operate(first,operation,second);
+                operation = '';
+            }
+        }
+        else if(buttons[i].innerText == '*' && text.value.toString() !== '')
+        {
+            if(operation == '' || typeof(operation) == 'undefined')
+            {
+                first = parseFloat(text.value);
+                isFirstRemoved = false;
+                operation = '*';
+            }
+            else
+            {
+                second = parseFloat(text.value);
+                text.value = calculator.operate(first,operation,second);
+                operation = '';
+            }
+        }
+        else if(buttons[i].innerText == '/' && text.value.toString() !== '')
+        {
+            if(operation == '' || typeof(operation) == 'undefined')
+            {
+                first = parseFloat(text.value);
+                isFirstRemoved = false;
+                operation = '/';
+            }
+            else
+            {
+                second = parseFloat(text.value);
+                text.value = calculator.operate(first,operation,second);
+                operation = '';
+            }
         }
         else if(buttons[i].innerText == 'clr')
         {
             text.value = "";
+            isFirstRemoved = false;
+            first = undefined;
+            second = undefined;
+            operation = '';
         }
-        else{
-        let input = buttons[i].innerText;
-        
-        text.value = text.value + input;
+        else
+        {
+            
+        input = buttons[i].innerText;
+
+        if(operation == '' || typeof(operation) == 'undefined')
+        {
+            text.value = text.value + input;
+        }
+        else
+        {
+            if(!isFirstRemoved)
+            {
+            text.value = '';
+            text.value = input;
+            isFirstRemoved = true;
+            }
+            else
+            {
+            text.value = text.value + input;
+            }
+        }
         }
     });
 }
